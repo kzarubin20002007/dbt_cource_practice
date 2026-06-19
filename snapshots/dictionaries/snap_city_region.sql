36eb5ac111a70ecc23c4622778e20b009c988e7e
Zarubin Kirill <KZarubin@croc.ru>
2026-06-19 17:00:36 +0300

    L5.5 HW

---
+++ snapshots/dictionaries/snap_city_region.sql
@@ -0,0 +1,27 @@
+{% snapshot snap_city_region %}
+
+{{
+   config(
+       target_schema='snapshot',
+       unique_key='city',
+
+       strategy='timestamp',
+       updated_at='updated_at',
+       dbt_valid_to_current="'9999-01-01'::date",
+
+       hard_deletes='new_record',
+
+       snapshot_meta_column_names={
+        "dbt_is_deleted": "dbt_is_deleted"
+      }
+   )
+}}
+
+SELECT 
+    city,
+    region,
+    updated_at 
+FROM 
+    {{ ref('city_region') }}
+
+{% endsnapshot %}
